beb5840857b329b8edb95a92374c0e200e8e322e
gaoyx <melo@gaoyuexiang.cn>
2017-05-18 13:49:46 +0800

    add fetch in signIn

--- src/signIn/SignIn.js
+++ src/signIn/SignIn.js
@@ -5,13 +5,50 @@ const mapStateToProps = (state) => {
   return state
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const fetchToSignIn = (signInUser) => dispatch => {
+  dispatch({type: 'SIGN_IN_PENDING'});
+  const postHeaders = new Headers({
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    'Content-Type': 'application/json'
+  });
+  const postInit = {
+    headers: postHeaders,
+    method: 'POST',
+    body: JSON.stringify(signInUser)
+  };
+  const request = new Request('http://localhost:8080/user', postInit);
+  fetch(request)
+    .then(response => {
+      console.log(response);
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error(response.json().msg);
+      }
+    })
+    .then(body => {
+      console.log(body);
+      dispatch({
+        type: 'SIGN_IN_SUCCESS',
+        msg: body.msg
+      })
+    })
+    .catch(error => {
+      dispatch({
+        type: 'SIGN_IN_FAILED',
+        msg: error.message
+      })
+    })
+};
+
+const mapDispatchToProps = (dispatch) => {
   return {
     signIn: (inputs) => {
-      dispatch({
-        type: 'SIGN_IN',
-        inputs
-      })
+      const signInUser = inputs.reduce((acc, next) => {
+        acc[next.key] = next.value;
+        return acc;
+      }, {});
+      dispatch(fetchToSignIn(signInUser))
     }
   }
 };
